d251295adebc6e171c8a39c10367e2d02b3875e7
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-10-24 22:12:59 -0400

    added button leading into prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
+document.getElementById("generate").addEventListener("click", questionsFunction);
+function questionsFunction () {
 var generateBtn = document.querySelector("#generate");
+var promptnumberofcharacter = window.prompt("How many characters do you want in your password? (Between 8 and 128");
+var promptupptercase = window.confirm("Do you want uppercase characters in your password?");
+var promptlowercase = window.confirm("Do you want lowercase characters in your password?");
+var promptsymbols = window.confirm("Do you want special characters in your password?");
+};
 
 // Write password to the #password input
 function writePassword() {
