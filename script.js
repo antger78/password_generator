348df9b94f5e949418f20dbba34551d8a013b83b
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-10-26 17:32:47 -0400

    working with tutor

--- script.js
+++ script.js
@@ -1,57 +1,133 @@
 // Assignment Code
-document.getElementById("generate").addEventListener("click", questionsFunction);
-function questionsFunction () {
 var generateBtn = document.querySelector("#generate");
-var promptnumberofcharacter = window.prompt("How many characters do you want in your password? (Between 8 and 128");
-var promptupptercase = window.confirm("I want uppercase characters in my password (Click cancel if not)");
-var promptlowercase = window.confirm("I want lowercase characters in my password (Click cancel if not)");
-var promptnumbers = window.confirm("I want numeric characters in my password (Click cancel if not)");
-var promptsymbols = window.confirm("I want special characters in my password (Click cancel if not)");
-};
 
+var promptnumberofcharacter;
+var promptupptercase;
+var promptlowercase;
+var promptnumbers;
+var promptsymbols;
 var upperarray = [
-  "A",
-  "B"
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
 ];
 
 var lowerarray = [
-  "a",
-  "b"
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
 ];
 
 var numericarray = [
-  "1",
-  "2"
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ];
 
 var symbolsarray = [
-  "!",
-  "@"
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
 ];
 
-if ( promptupptercase == true)
-{
+let passwordcharacterset = [];
+// document.getElementById("generate").addEventListener("click", questionsFunction);
+function questionsFunction () {
+promptnumberofcharacter = parseInt (window.prompt("How many characters do you want in your password? (Between 8 and 128"));
+if (Number.isNaN(promptnumberofcharacter)){
+  window.alert ("Enter an integer value between 8 and 128");
+  return "";
+}
+if (promptnumberofcharacter < 8){
+  window.alert ("Enter an integer value between 8 and 128");
+  return "";
+}
+if (promptnumberofcharacter > 128){
+  window.alert ("Enter an integer value between 8 and 128");
+  return "";
+}
+console.log(promptnumberofcharacter);
+promptuppercase = window.confirm("I want uppercase characters in my password (Click cancel if not)");
+if (promptuppercase){
+  passwordcharacterset.concat(upperarray);
+  console.log (passwordcharacterset);
+};
+promptlowercase = window.confirm("I want lowercase characters in my password (Click cancel if not)");
+promptnumbers = window.confirm("I want numeric characters in my password (Click cancel if not)");
+promptsymbols = window.confirm("I want special characters in my password (Click cancel if not)");
+};
+
+function generatePassword(){
+  questionsFunction();
+};
+
+for (let i = 1; i > promptnumberofcharacter; randomlyPickFromSelected){
 
 }
-else{}
-
-if ( promptlowercase == true)
-{
-  
-}
-else{}
-
-if ( promptnumbers == true)
-{
-  
-}
-else{}
-
-if ( promptsymbols == true)
-{
-  
-}
-else{}
 
 // Write password to the #password input
 function writePassword() {
@@ -60,7 +136,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -70,5 +146,89 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-var randomUpper = upperarray[Math.floor(Math.random()*upperarray.length)];
+// var randomUpper = upperarray[Math.floor(Math.random()*upperarray.length)];
+// // if ( promptlowercase == true)
+// {
+  
+// }
+// else{};
+
+// if ( promptnumbers == true)
+// {
+  
+// }
+// else{};
+
+// if ( promptsymbols == true)
+// {
+  
+// }
+// else{};
+
+
+// if ( promptupptercase == false && promptlowercase == false && promptnumbers == false && promptsymbols == false)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == true && promptlowercase == true && promptnumbers == true && promptsymbols == true)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == true && promptlowercase == false && promptnumbers == true && promptsymbols == true)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == true && promptlowercase == false && promptnumbers == false && promptsymbols == true)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == true && promptlowercase == false && promptnumbers == false && promptsymbols == false)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == false && promptlowercase == true && promptnumbers == true && promptsymbols == true)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == false && promptlowercase == true && promptnumbers == false && promptsymbols == true)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == false && promptlowercase == true && promptnumbers == false && promptsymbols == false)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == false && promptlowercase == false && promptnumbers == true && promptsymbols == true)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == false && promptlowercase == false && promptnumbers == true && promptsymbols == false)
+// {
+
+// }
+// else{};
+
+// if ( promptupptercase == false && promptlowercase == false && promptnumbers == false && promptsymbols == true)
+// {
+
+// }
+// else{};
 
